2d9798c739d314642bd4fe4997936d21bfa95f4f
srinivasch2202 <131776805+srinivasch2202@users.noreply.github.com>
2023-07-10 18:59:27 +0530

    Delete java directory

--- java/inpur.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class deo{
-    public static void main(String args[]){
-        System.out.println("hello");
-        Scanner input =new Scanner(System.in);
-        System.out.println(input.nextLine());
-    }
-}
--- java/loop1.java
+++
@@ -1,9 +0,0 @@
-public class loop1 {
-    public static void main(String args[]){
-        int i=5;
-        for(int j=0;j<i;j++){
-            System.out.println(j);
-        }
-    }
-    
-}
